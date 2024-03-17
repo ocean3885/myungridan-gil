f75ab7e4e5c6ae82fa936d6981a34bec2a3aa0df
ocean3885 <ocean3885@gmail.com>
2024-03-17 18:58:53 +0900

    whitenoise install

--- myungridangil/settings.py
+++ myungridangil/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'whitenoise.runserver_nostatic',
     'manseryuk',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
