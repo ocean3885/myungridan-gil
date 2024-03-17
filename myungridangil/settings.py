30badda3c4acaf391ee85581d61faa5d6e58a5d4
ocean3885 <ocean3885@gmail.com>
2024-03-17 19:39:04 +0900

    static modify try 6

--- myungridangil/settings.py
+++ myungridangil/settings.py
@@ -40,13 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'whitenoise',
+    # 'whitenoise',
     'manseryuk',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +132,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/" 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [ BASE_DIR / 'static', ]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
