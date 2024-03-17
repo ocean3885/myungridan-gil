ef6c27fefeb6bbe8a2cb1252ba3668ded0d69d15
ocean3885 <ocean3885@gmail.com>
2024-03-17 20:10:57 +0900

    static modify try 9

--- myungridangil/settings.py
+++ myungridangil/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/" 
-STATIC_ROOT = '/var/www/myungridangil'
+STATIC_ROOT = '/var/www/myungridangil/static'
 STATICFILES_DIRS = [ BASE_DIR / 'static', ]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
