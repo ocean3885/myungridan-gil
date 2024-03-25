e2fd87b6e9baae728eb0fc13e08b861536fd2223
ocean3885 <ocean3885@gmail.com>
2024-03-25 15:34:42 +0900

    not apply css modify try 1

--- myungridangil/settings.py
+++ myungridangil/settings.py
@@ -137,8 +137,11 @@ STATICFILES_DIRS = [ BASE_DIR / 'static', ]
 
 COMPRESS_ROOT = BASE_DIR / 'static'
 COMPRESS_ENABLED = True
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
-
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+    )
 
 
 # Default primary key field type
