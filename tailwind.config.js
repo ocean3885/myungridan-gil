8f0562047d035421a68f32414a646fc58f3d4c4a
ocean3885 <ocean3885@gmail.com>
2024-03-28 15:54:59 +0900

    remove compressor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,14 @@ module.exports = {
   content: [
     './templates/**/*.html',
     './node_modules/flowbite/**/*.js',
-    'node_modules/preline/dist/*.js',
+    './node_modules/preline/dist/*.js',
   ],
   theme: {
     fontFamily: {
       'kor-nanum': ['"NanumGothic"',],
       'notosanskr': ['Noto Sans Kr',],
     },
-    extend: {
-      utilities: {
-        '.webkit-overflow-scrolling-none': {
-          '-webkit-overflow-scrolling': 'none',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [
     require('flowbite/plugin'),
