0720edefdbe7222500ad94898a6497238bfdc02d
ocean3885 <ocean3885@gmail.com>
2024-04-15 17:24:26 +0900

    jm_submit page ing

---
+++ submit/forms.py
@@ -0,0 +1,64 @@
+from django.forms import ModelForm
+from django import forms
+import datetime
+from .models import Submit, Person
+
+class JmSubmitForm(ModelForm):
+
+    field_order = ['process', 'name', 'phone', 'visit', 'wantdate', 'email', 'adress', 'first_name_ch', 
+                   'first_name_kr', 'first_name_bon', 'fav_name', 'avoid_name',
+                   'dad_name', 'mom_name', 'dolrim', 'description']
+
+    class Meta:
+        model = Submit
+        exclude = ['category', 'user']
+
+
+class PersonForm(ModelForm):
+
+    today = datetime.date.today()
+    YEAR_CHOICES = []
+    for r in range(1940, (today.year+1)):
+        YEAR_CHOICES.append((r, r))
+    YEAR_CHOICES.reverse()
+    year = forms.ChoiceField(widget=forms.Select, choices=YEAR_CHOICES,
+                             initial=today.year, label="태어난 년(年) ", required=True)
+
+    MONTH_CHOICES = []
+    for r in range(1, 13):
+        MONTH_CHOICES.append((r, r))
+    month = forms.ChoiceField(widget=forms.Select, choices=MONTH_CHOICES,
+                              initial=today.month, label="태어난 월(月) ")
+
+    DAY_CHOICES = []
+    for r in range(1, 32):
+        DAY_CHOICES.append((r, r))
+
+    day = forms.ChoiceField(widget=forms.Select, choices=DAY_CHOICES,
+                            initial=today.day, label="태어난 일(日) ")
+
+    GEN_CHOICES = [("남", "남"), ("여", "여")]
+    gen = forms.ChoiceField(widget=forms.Select,
+                            choices=GEN_CHOICES, label="성 별 ")
+    SL_CHOICES = [("양력", "양력"), ("음력", "음력"), ("음력윤달", "음력윤달")]
+    sl = forms.ChoiceField(widget=forms.Select,
+                           choices=SL_CHOICES, label="양력/음력 ")
+    
+    HOUR_CHOICES = []
+    for r in range(0, 24):
+        HOUR_CHOICES.append((r, r))
+
+    hour = forms.ChoiceField(widget=forms.Select, choices=HOUR_CHOICES,
+                            label="시간")
+    
+    MIN_CHOICES = []
+    for r in range(0,60):
+        MIN_CHOICES.append((r, r))
+
+    min = forms.ChoiceField(widget=forms.Select, choices=MIN_CHOICES,
+                            label="분")
+    
+
+    class Meta:
+        model = Person
+        exclude = ['submit', 'name']
--- submit/views.py
+++ submit/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
+from .forms import (JmSubmitForm, PersonForm)
+import requests
 
+send_url = 'https://apis.aligo.in/send/'
 
 def jm_submit(request):
-    return render(request, 'submit/jm_submit.html')
+    if request.method == "POST":
+        submitForm = JmSubmitForm(request.POST)
+        personForm = PersonForm(request.POST)
+        if all([submitForm.is_valid(), personForm.is_valid()]):
+            obj = submitForm.save(commit=False)
+            obj.category = "jm"
+            if request.user.is_authenticated:
+                obj.user = request.user
+            obj.save()
+            person = personForm.save(commit=False)
+            person.submit = obj
+            person.save()
+            context = {'submit': obj, 'person': person}
+            sms_data = {'key': 'mbam9e8v586xu9vugol89i2wxvihrv9l',
+                        'userid': 'ocean3885',
+                        'sender': '01022324548',
+                        'receiver': '01022324548',
+                        'msg': '작명신청이 접수되었습니다.'
+                        }
+            send_response = requests.post(send_url, data=sms_data)
+            print(send_response.json())
+            return render(request, 'submit/submit_complete.html', context)
+        else:
+            context = {'submit': submitForm, 'person': personForm}
+            return render(request, 'submit/jm_submit.html', context)
+
+    submitForm = JmSubmitForm()
+    personForm = PersonForm()
+    context = {'submit': submitForm, 'person': personForm}
+    return render(request, 'submit/jm_submit.html', context)
+
+
 
 def gm_submit(request):
     return render(request, 'submit/gm_submit.html')
