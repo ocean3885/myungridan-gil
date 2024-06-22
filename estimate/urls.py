114d85fecaed0f61dc669e8ce43701d15833d314
ocean3885 <ocean3885@gmail.com>
2024-06-22 16:41:58 +0900

    estimate url edit

--- estimate/urls.py
+++ estimate/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('form/', views.estimate_form, name="estimate-form"),
+    path('', views.estimate_form, name="estimate-form"),
     path('list/', views.estimate_list, name="estimate-list"),
     path('detail/<int:pk>/', views.estimate_detail, name="estimate-detail"),
     path('detail/edit/<int:pk>/', views.estimate_edit, name="estimate-edit"),
