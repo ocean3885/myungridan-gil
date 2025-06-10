18f91817204591c45a2dbde035b98e00d03cc3f2
ocean3885 <ocean3885@naver.com>
2025-06-10 18:04:55 +0900

    edit base admin.py

--- base/admin.py
+++ base/admin.py
@@ -9,8 +9,14 @@ class CustomCommentInline(admin.TabularInline):
 
 class CustomBoardAdmin(admin.ModelAdmin):
     inlines = [CustomCommentInline]  # CustomBoard 내에서 CustomComment를 표시
+    
+class ScheduledItemAdmin(admin.ModelAdmin):
+    """ScheduledItem 모델을 위한 관리자 페이지 설정"""
+
+    # 2. 목록 페이지에 보여줄 필드들을 지정합니다.
+    list_display = ('post_name', 'post_title', 'post_is_secret', 'status')
 
 # 모델을 등록합니다.
 admin.site.register(Profile)
 admin.site.register(CustomBoard, CustomBoardAdmin)
-admin.site.register(ScheduledItem)
+admin.site.register(ScheduledItem, ScheduledItemAdmin)
