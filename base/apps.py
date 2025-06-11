09f9d3b1bf87e1a246f1f520f2afaf47a054d995
ocean3885 <ocean3885@naver.com>
2025-06-11 09:16:55 +0900

    table and no darkmode update

--- base/apps.py
+++ base/apps.py
@@ -7,5 +7,6 @@ class BaseConfig(AppConfig):
     def ready(self):
     # Django가 준비되면 스케줄러를 시작합니다.
     # 개발 서버(runserver)는 리로딩 때문에 코드가 두 번 실행될 수 있습니다.
-        from . import scheduler
-        scheduler.start()
+        # from . import scheduler
+        # scheduler.start()
+        pass
